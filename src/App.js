e5fc4458f83297bcdc934a39c491b88d76a64e47
Muneeb Ahmad <54245615+ahmaddmuneeb@users.noreply.github.com>
2020-10-03 04:50:48 +0500

    PWA Completed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-//Importing the API
+// Importing the API
 import { fetchWeather } from "./api/fetchWeather";
 
 const App = () => {
+
+    const [query, setQuery] = useState('');
+    // Set state for the use of weather data
+    const [weather, setWeather] = useState({});
+
+    const search = async (e) => {
+        if (e.key === 'Enter')
+        {
+            const data = await fetchWeather(query);
+            // Getting data from the API
+            setWeather(data);
+            // Just to reset input "Search" after enter
+            setQuery('');
+        }
+    }
+
   return (
       <div className="main-container">
         <input
             type="text"
             className="search"
             placeholder="Search..."
-            value={}
-            onChange={}
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            onKeyPress={search}
         />
+          {weather.main && (
+              <div className="city">
+                  <h2 className="city-name">
+                     <span>{weather.name}</span>
+                      <sup>{weather.sys.country}</sup>
+                  </h2>
+                  <div className="city-temp">
+                      {Math.round(weather.main.temp)}
+                      <sup>&deg;C</sup>
+                  </div>
+                  <div className="info">
+                      <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+                      <p>{weather.weather[0].description}</p>
+                  </div>
+              </div>
+          )}
       </div>
   );
 }
--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = 'http://api.openweathermap.org/data/2.5/weather';
+const URL = 'https://api.openweathermap.org/data/2.5/weather?';
 const API_KEY = '03146cd51a998bb75e9939cfe72a7515';
 
 export const fetchWeather = async (query) => {
@@ -10,7 +10,7 @@ export const fetchWeather = async (query) => {
         params: {
             q: query,
             units: 'metric',
-            APP_ID: API_KEY,
+            APPID: API_KEY,
         }
     });
     return data;
